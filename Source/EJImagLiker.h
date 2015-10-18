142d1152a2ac72b4208489e0acf600fd33764743
Ehsan Jahromi <ehsan_eshragh@yahoo.com>
2015-10-18 04:17:15 -0700

    Source files added

---
+++ Source/EJImagLiker.h
@@ -0,0 +1,31 @@
+//
+//  EJImagLiker.h
+//  
+//
+//  Created by Ehsan Jahromi on 10/17/15.
+//  Copyright © 2015 Ehsan Jahromi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+typedef NS_ENUM(NSUInteger, AnimationStyle) {
+    AnimationStyleFade = 0,
+    AnimationStyleMoveToCorner,
+    AnimationStyleStayOnBottom,
+    AnimationStyleBlink
+};
+
+typedef NS_ENUM(NSUInteger, LikerColor) {
+    LikerColorWhite = 0,
+    LikerColorBlack
+};
+
+@interface ImgLiker : UIImageView
+
+@property (nonatomic) enum AnimationStyle animationStyle;
+@property (nonatomic) enum LikerColor likerImageColor;
+
+
+-(instancetype)initWithFrame:(CGRect)frame andImage:(UIImage *)image;
+
+@end
